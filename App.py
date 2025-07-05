928cba75dcc2730fcfa485b635d817b42fbfd77b
Elicarus <37748207+Elicarus@users.noreply.github.com>
2025-07-05 16:40:36 +0200

    Update App.py

--- App.py
+++ App.py
@@ -3,8 +3,6 @@ from PyQt5.QtWidgets import QDesktopWidget, QApplication
 from PyQt5.QtWebEngineWidgets import QWebEngineView
 import os
 import sys
-
-from pathlib import Path
 #import streamlit as st
 
 def main() :
