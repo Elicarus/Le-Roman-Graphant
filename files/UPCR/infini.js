a48535cf9165b3519d1e6fe5abbc84813bd70e70
Elicarus <37748207+Elicarus@users.noreply.github.com>
2025-07-09 21:01:38 +0200

    Delete files/UPCR directory

--- files/UPCR/infini.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener( 'scroll', function () {
-  var d = document.getElementById( 'endless' );
-  texte = "\nd'un homme amoureux, \npicorant au hasard \ncomme un poulet sans tête, \nramassant les miettes\n"
-  d.innerText = d.innerText + texte + texte + texte
-});
