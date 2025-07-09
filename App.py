3a743b4cabec773624ffff1e42fe341686460f42
Elicarus <37748207+Elicarus@users.noreply.github.com>
2025-07-09 21:13:54 +0200

    Update App.py

--- App.py
+++ App.py
@@ -3,7 +3,7 @@ import os
 
 @st.cache_resource
 def get_file() : 
-    return open(os.path.join("final version", "Roman Graphant.zip"), 'rb')
+    return open("Roman Graphant.zip", 'rb')
 
 f = get_file()
 st.download_button("Roman Graphant.zip", data=f, file_name="Roman Graphant.zip", on_click=lambda : st.write("Have fun!!"))
