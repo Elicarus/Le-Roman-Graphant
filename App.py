f92e44b57ec6559eb2e67942e20a6d36d6b8c1a3
Elicarus <37748207+Elicarus@users.noreply.github.com>
2025-07-09 21:18:32 +0200

    Update App.py

--- App.py
+++ App.py
@@ -6,4 +6,5 @@ def get_file() :
     return open("Roman Graphant.zip", 'rb')
 
 f = get_file()
-st.download_button("Roman Graphant.zip", data=f, file_name="Roman Graphant.zip", on_click=lambda : st.write("Have fun!!"))
+st.write("Bon ça marche que sur windows mais tkt")
+st.download_button("Roman Graphant.zip", data=f, file_name="Roman Graphant.zip", on_click=lambda : st.markdown("et PAF allez tu mdis si c'est bien <a href='mailto:tomwainstain@gmail.com'>ici</a>", unsafe_allow_html=True))
